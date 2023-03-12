d448df0236d88b996e24e62396ae8f6e2598d31a
Lucas Martins de Freitas <lucasmartinsde@gmail.com>
2023-03-12 17:55:56 -0300

    first commit

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('blog::layouts.master')
+
+@section('content')
+    <h1>Olá esse é o modulo feito por mim Lucas</h1>
+
+    <p>
+        View vinda do modulos: {!! config('blog.name') !!}
+    </p>
+@endsection
